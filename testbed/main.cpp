79701d655202b2accb11437f388da7f4d27f259e
Awcmon <adcarbine@yahoo.com>
2016-01-24 20:22:08 -0800

    Implemented Shader class in prev commit.

--- testbed/main.cpp
+++ testbed/main.cpp
@@ -73,25 +73,6 @@ int main(int argc, char* args[])
 		0, 1, 2,
 		1, 2, 3
 	};
-	/*
-	// Create and compile the vertex shader
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
-	glCompileShader(vertexShader);
-
-	// Create and compile the fragment shader
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-	glCompileShader(fragmentShader);
-
-	// Link the vertex and fragment shader into a shader program
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glBindFragDataLocation(shaderProgram, 0, "outColor");
-	glLinkProgram(shaderProgram);
-	glUseProgram(shaderProgram);
-	*/
 
 	Shader shaderProgram(vertexSource, fragmentSource);
 	shaderProgram.use();
