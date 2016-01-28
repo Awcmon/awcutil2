9b5c113062f8990e89ee0de4bba6534f31cb24d7
Awcmon <adcarbine@yahoo.com>
2016-01-28 00:05:16 -0800

    Shader set uniform functions

--- awcutil2/Shader.cpp
+++ awcutil2/Shader.cpp
@@ -57,5 +57,60 @@ namespace awcutil
 			return *this;
 		}
 
+		void Shader::setFloat(const GLchar *name, GLfloat value, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform1f(glGetUniformLocation(id, name), value);
+		}
+		void Shader::setInteger(const GLchar *name, GLint value, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform1i(glGetUniformLocation(id, name), value);
+		}
+		void Shader::setVector2f(const GLchar *name, GLfloat x, GLfloat y, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform2f(glGetUniformLocation(id, name), x, y);
+		}
+		void Shader::setVector2f(const GLchar *name, const glm::vec2 &value, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform2f(glGetUniformLocation(id, name), value.x, value.y);
+		}
+		void Shader::setVector3f(const GLchar *name, GLfloat x, GLfloat y, GLfloat z, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform3f(glGetUniformLocation(id, name), x, y, z);
+		}
+		void Shader::setVector3f(const GLchar *name, const glm::vec3 &value, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform3f(glGetUniformLocation(id, name), value.x, value.y, value.z);
+		}
+		void Shader::setVector4f(const GLchar *name, GLfloat x, GLfloat y, GLfloat z, GLfloat w, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform4f(glGetUniformLocation(id, name), x, y, z, w);
+		}
+		void Shader::setVector4f(const GLchar *name, const glm::vec4 &value, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniform4f(glGetUniformLocation(id, name), value.x, value.y, value.z, value.w);
+		}
+		void Shader::setMatrix4(const GLchar *name, const glm::mat4 &matrix, GLboolean useShader)
+		{
+			if (useShader)
+				use();
+			glUniformMatrix4fv(glGetUniformLocation(id, name), 1, GL_FALSE, glm::value_ptr(matrix));
+		}
+
 	}
 }
--- awcutil2/Shader.h
+++ awcutil2/Shader.h
@@ -20,6 +20,16 @@ namespace awcutil
 			~Shader();
 			Shader &use();
 
+			void    setFloat(const GLchar *name, GLfloat value, GLboolean useShader = false);
+			void    setInteger(const GLchar *name, GLint value, GLboolean useShader = false);
+			void    setVector2f(const GLchar *name, GLfloat x, GLfloat y, GLboolean useShader = false);
+			void    setVector2f(const GLchar *name, const glm::vec2 &value, GLboolean useShader = false);
+			void    setVector3f(const GLchar *name, GLfloat x, GLfloat y, GLfloat z, GLboolean useShader = false);
+			void    setVector3f(const GLchar *name, const glm::vec3 &value, GLboolean useShader = false);
+			void    setVector4f(const GLchar *name, GLfloat x, GLfloat y, GLfloat z, GLfloat w, GLboolean useShader = false);
+			void    setVector4f(const GLchar *name, const glm::vec4 &value, GLboolean useShader = false);
+			void    setMatrix4(const GLchar *name, const glm::mat4 &matrix, GLboolean useShader = false);
+
 			GLuint id;
 
 		};
